496643b23059dd0d3e4085dc92b0bda66932b580
brain talked <69191373+braintalked@users.noreply.github.com>
2021-06-03 21:43:16 +0300

    Update addNewClientToSaloonDashboard.js

--- authorised/addNewClientToSaloonDashboard.js
+++ authorised/addNewClientToSaloonDashboard.js
@@ -5,7 +5,6 @@
               let clientsIsEmpty = localStorage.getItem("clientsIsEmpty");
               let saloonID = localStorage.getItem("saloonID");
               document.querySelector("tbody").id = saloonID;
-              console.log("haha "+saloonID);
               //clients
               let parsedSaloonClients = []
               
@@ -53,10 +52,23 @@
 
               db.collection("clients").onSnapshot((snapshot) => {
                   let changes  = snapshot.docChanges();
+                  if (changes == "")
+                  {
+                    loader.classList.add("hidden");
+                    loader.classList.add("d-none");
+                  }
+                  
+                  console.log(snapshot);
+
                   changes.forEach(change => {
                   let  changeType = change.type; 
-                  console.log(changeType);
                   let data = change.doc.data();
+
+                  if(Object.keys(data).length == 0 || data[saloonID] == null )
+                  {
+                    loader.classList.add("hidden");
+                    loader.classList.add("d-none"); 
+                  }
                     if((changeType == "modified" || changeType == "added"))
                     {
                       // if (document.querySelectorAll(".newClientRow").length > 0)
@@ -67,7 +79,6 @@
                           
                       //       });
                       //     }
-                      console.log("inside the loop");
                      
                       let counter = 0;
                       for (var key in data)
@@ -82,6 +93,8 @@
                         }
                         if((data[key]["saloonID"] == firebase.auth().currentUser.uid))
                         {
+                          let clientID = data[key]["clientID"];
+                          console.log(clientID);
                           let saloonID = data[key]["saloonID"];
                           let clientName = data[key]["clientName"];
                           let clientPhone =  data[key]["clientPhone"];
@@ -91,39 +104,62 @@
                           console.log(clientName);
                           console.log(clientPhone);
                           console.log(clientBookedServices);
-                          addNewClient(clientName, clientPhone, clientBookedServices);
+                          console.log(clientID);
+                          addNewClient(clientName, clientPhone, clientBookedServices, clientID);
                           console.log(data[key]);
                         }
                        
                       }
     
-                    } 
+                    }
                 })
-              })
+              }, (error) => {
+                console.log("error getting data");
+            });
             ////////////////////// End of Real-time database listener ///////////////////////
   
-let i = 1;
-function addNewClient(clientName, clientPhone, clientBookedServices)
+function addNewClient(clientName, clientPhone, clientBookedServices, clientID)
 {
   loader.classList.add("hidden");
   loader.classList.add("d-none");
 
   console.log("inside addNewClient Function...");
 
-      let tr = document.createElement("tr");
-      let td1 = document.createElement("td");
-      let td2 = document.createElement("td");
-      let td3 = document.createElement("td");
-      let td4 = document.createElement("td");
-      let td5 = document.createElement("td");
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  let td3 = document.createElement("td");
+  let td4 = document.createElement("td");
+  let td5 = document.createElement("td");
 
-      td1.innerText = "";
-      td2.innerText =""+clientName;
-      td3.innerText = ""+clientPhone;
-      td4.innerText = ""+clientBookedServices;
-      td5.innerText = "";
+  let i = 1;
+
+  if (i<=20)
+  {
+
+      let button = document.createElement("button");
+
+      button.type = 'button';
+      button.border = "none";
+      button.id = "removeClientButton";
+      button.style.backgroundColor = "rgb(236, 79, 79)";
+      button.style.width = "30px";
+      button.style.height = "30px";
+      button.style.display="flex";
+      button.style.justifyContent = "center";
+      button.style.alignItems ="center";
+      button.innerHTML = "<img src='images/remove.png'></img>";
+
+      td1.innerText =""+clientName;
+      td2.innerText = ""+clientPhone;
+      td3.innerText = ""+clientBookedServices;
+      td4.innerText = ""; // Time and date cell
+      td5.style.boxShadow  = "none";
+      td5.style.width = "1%";
+      td5.style.padding = "0";
       tr.classList.add("newClientRow");
 
+      td5.appendChild(button);
       tr.appendChild(td1);
       tr.appendChild(td2);
       tr.appendChild(td3);
@@ -137,4 +173,21 @@ function addNewClient(clientName, clientPhone, clientBookedServices)
 
       var appendingElement = document.getElementById("startAppendingClientsFromHere")
       insertAfter(appendingElement,tr);
+
+      i = i + 1;
+    }
+
+  document.getElementById("removeClientButton").addEventListener('click',function(){
+    
+    let i = 1;
+    tr.classList.add("d-none");
+    tr.classList.remove("clientTableRow");
+    i = i-1;
+
+    // Remove client from the "clients" database...
+    var clientsRef = db.collection('clients').doc(saloonID).set({
+      [clientID] : {}
+    }, { merge: true });
+  });
+ 
 }
